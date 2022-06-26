97a9683d5adf57fdf8d39e8eabb250ec22399416
un-likely <108141279+un-likely@users.noreply.github.com>
2022-06-26 02:46:37 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header({setSignOpen}) {
   return ( 
         <StyledHeader>
             <div className="navbar_container">
-                <div id="navbar_logo"><img src='imgs/Logo.png' alt='TryItOn Logo'></img></div>
+                <div id="navbar_logo"><img src='imgs/LogoSmall.png' alt='TryItOn Logo'></img></div>
                 <ul className="navbar_manu">
                     <li className="nav-links">
                         <Link className="navbar_links" to="/">men</Link>
